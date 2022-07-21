80f3ad2f9031508c8ba5de5779f3f73e56527596
枫阿雨 <2522235302@qq.com>
2022-07-21 10:24:58 +0800

    服务列表API

---
+++ controller/service.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"github.com/e421083458/FayGateway/dto"
+	"github.com/e421083458/FayGateway/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+type ServiceController struct{}
+
+func ServiceRegister(group *gin.RouterGroup) {
+	service := &ServiceController{}
+	group.GET("/service_list", service.ServiceList)
+}
+
+// ServiceList godoc
+// @Summary 服务列表
+// @Description 服务列表
+// @Tags 服务管理
+// @ID /service/service_list
+// @Accept  json
+// @Produce  json
+// @Param info query string false "关键词"
+// @Param page_size query int true "每页个数"
+// @Param page_no query int true "当前页数"
+// @Success 200 {object} middleware.Response{data=dto.ServiceListOutput} "success"
+// @Router /service//service_list [get]
+func (service *ServiceController) ServiceList(c *gin.Context) {
+	params := &dto.ServiceListInput{}
+	if err := params.BindValidParam(c); err != nil {
+		middleware.ResponseError(c, 2000, err)
+		return
+	}
+
+	out := &dto.ServiceListOutput{}
+	middleware.ResponseSuccess(c, out)
+}
--- router/route.go
+++ router/route.go
@@ -99,5 +99,16 @@ func InitRouter(middlewares ...gin.HandlerFunc) *gin.Engine {
 	{
 		controller.AdminRegister(adminRouter)
 	}
+
+	serviceRouter := router.Group("/service")
+	serviceRouter.Use(
+		sessions.Sessions("mysession", store),
+		middleware.RecoveryMiddleware(),
+		middleware.RequestLog(),
+		middleware.SessionAuthMiddleware(),
+		middleware.TranslationMiddleware())
+	{
+		controller.ServiceRegister(serviceRouter)
+	}
 	return router
 }
