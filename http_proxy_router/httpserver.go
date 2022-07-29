fd72c60c6829ecb0d486699c90bcf37d2292f349
枫阿雨 <2522235302@qq.com>
2022-07-29 11:31:39 +0800

    HTTP与HTTPS代理API完成

---
+++ http_proxy_router/httpserver.go
@@ -0,0 +1,72 @@
+package http_proxy_router
+
+import (
+	"context"
+	"github.com/e421083458/FayGateway/cert_file"
+	"github.com/e421083458/golang_common/lib"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"time"
+)
+
+var (
+	HttpSrvHandler  *http.Server
+	HttpsSrvHandler *http.Server
+)
+
+func HttpServerRun() {
+	gin.SetMode(lib.GetStringConf("proxy.base.debug_mode"))
+	r := InitRouter()
+	HttpSrvHandler = &http.Server{
+		Addr:           lib.GetStringConf("proxy.http.addr"),
+		Handler:        r,
+		ReadTimeout:    time.Duration(lib.GetIntConf("proxy.http.read_timeout")) * time.Second,
+		WriteTimeout:   time.Duration(lib.GetIntConf("proxy.http.write_timeout")) * time.Second,
+		MaxHeaderBytes: 1 << uint(lib.GetIntConf("proxy.http.max_header_bytes")),
+	}
+
+	log.Printf(" [INFO] Http_proxy_Run:%s\n", lib.GetStringConf("proxy.http.addr"))
+	if err := HttpSrvHandler.ListenAndServe(); err != nil {
+		log.Fatalf(" [ERROR] Http_proxy_Run:%s err:%v\n", lib.GetStringConf("proxy.http.addr"), err)
+	}
+
+}
+
+func HttpServerStop() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := HttpSrvHandler.Shutdown(ctx); err != nil {
+		log.Fatalf(" [ERROR] HttpServerStop err:%v\n", err)
+	}
+	log.Printf(" [INFO] Http_proxy_stop stopped\n")
+}
+
+func HttpsServerRun() {
+	gin.SetMode(lib.GetStringConf("proxy.base.debug_mode"))
+	r := InitRouter()
+	HttpsSrvHandler = &http.Server{
+		Addr:           lib.GetStringConf("proxy.https.addr"),
+		Handler:        r,
+		ReadTimeout:    time.Duration(lib.GetIntConf("proxy.https.read_timeout")) * time.Second,
+		WriteTimeout:   time.Duration(lib.GetIntConf("proxy.https.write_timeout")) * time.Second,
+		MaxHeaderBytes: 1 << uint(lib.GetIntConf("proxy.https.max_header_bytes")),
+	}
+
+	log.Printf(" [INFO] Https_proxy_Run:%s\n", lib.GetStringConf("proxy.https.addr"))
+	//log.Fatal(http.ListenAndServeTLS(addr, testdata.Path("server.crt"), testdata.Path("server.key"), proxy))
+
+	if err := HttpsSrvHandler.ListenAndServeTLS(cert_file.Path("server.crt"), cert_file.Path("server.key")); err != nil {
+		log.Fatalf(" [ERROR] Https_proxy_Run:%s err:%v\n", lib.GetStringConf("proxy.https.addr"), err)
+	}
+
+}
+
+func HttpsServerStop() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := HttpsSrvHandler.Shutdown(ctx); err != nil {
+		log.Fatalf(" [ERROR] HttpsServerStop err:%v\n", err)
+	}
+	log.Printf(" [INFO] Https_proxy_stop stopped\n")
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"github.com/e421083458/FayGateway/http_proxy_router"
 	"github.com/e421083458/FayGateway/router"
 	"github.com/e421083458/golang_common/lib"
 	"os"
@@ -8,14 +10,51 @@ import (
 	"syscall"
 )
 
+//endpoint dashboard后台管理 server代理服务器
+//conf ./conf/prod/ 对应配置文件夹
+
+var (
+	endpoint = flag.String("endpoint", "", "input endpoint dashboard or server")
+	config   = flag.String("conf", "", "input config file like ./conf/dev/")
+)
+
 func main() {
-	lib.InitModule("./conf/dev/", []string{"base", "mysql", "redis"})
-	defer lib.Destroy()
-	router.HttpServerRun()
+	flag.Parse()
 
-	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
+	if *endpoint == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *config == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *endpoint == "dashboard" {
+		lib.InitModule(*config, []string{"base", "mysql", "redis"})
+		defer lib.Destroy()
+		router.HttpServerRun()
 
-	router.HttpServerStop()
+		quit := make(chan os.Signal)
+		signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+
+		router.HttpServerStop()
+	} else {
+		lib.InitModule(*config, []string{"base", "mysql", "redis"})
+		defer lib.Destroy()
+		go func() {
+			http_proxy_router.HttpServerRun()
+		}()
+		go func() {
+			http_proxy_router.HttpsServerRun()
+		}()
+
+		quit := make(chan os.Signal)
+		signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+
+		http_proxy_router.HttpsServerStop()
+		http_proxy_router.HttpServerStop()
+	}
+
 }
